c2e3b2d107b2daa7e65d6f7f1cb6f1e7a2ab868d
Conny <kawohl.bentodesign@gmail.com>
2017-01-11 11:17:03 +0100

    prints something out

--- src/core.c
+++ src/core.c
@@ -4,6 +4,14 @@ int main(void)
 {
     int  n;
 
+
+	printf("--- --- --- --- ---\n");
+	printf(" SYMANTHA CORE 0.0.1 \n");
+	printf("--- --- --- --- ---\n");
+
+
+
+
     while (scanf("%d", &n) == 1)
         printf("%d\n", n);
     return 0;
